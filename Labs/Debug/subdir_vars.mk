f02bf3d40316b5aed3cb1b2c3ca4b74066e755dd
Matthew Teears <mteears@eagles.ewu.edu>
2021-01-07 15:26:19 -0800

    Support for TIVA board, relocated files.  checking function

---
+++ Labs/Debug/subdir_vars.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/ccs/Debug/driverlib.lib 
+
+ASM_SRCS += \
+../lab1.asm 
+
+C_SRCS += \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_DEPS += \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lab1.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+ASM_DEPS += \
+./lab1.d 
+
+OBJS__QUOTED += \
+"lab1.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"tm4c123gh6pm_startup_ccs.d" 
+
+ASM_DEPS__QUOTED += \
+"lab1.d" 
+
+ASM_SRCS__QUOTED += \
+"../lab1.asm" 
+
+C_SRCS__QUOTED += \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
