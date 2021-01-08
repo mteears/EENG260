af555035135be2fd26ff8231ff668b95decf51de
Matthew Teears <mteears@eagles.ewu.edu>
2021-01-07 16:04:24 -0800

    Finished Lab 1. Added CMP before BNE for function.  Results in 0x78 = 120 in deciamal, which is correct

--- extra/Lab1/example.asm
+++ extra/Lab1/example.asm
@@ -11,22 +11,19 @@ Prog_Start:	MOVW		R1,#0x0400	;puts 0x0400 in Register 1. [15:00] 	(0x----0400)
 			MOVT		R1,#0x2000	;puts 0x2000 in Register 1. [31:16]		(0x20000400) {address}
 			MOVB		R0,#0x00	;puts 0x0000 in Register 0. (0x0000)
 			MOVB		R2,#0x10	;puts 0x0010 in Register 2. (0x0010)
-			MOVW		R3,#0x0000	;puts 0x0000 in Register 3. (0x0000)
-			MOVB		R4,#0x02	;puts 0x0002 in Register 4.
 Loop:		STRB		R0,[R1]		;puts LSB from R0 into address at R1	(0x0000 -> 0x20000400) {first time}
-			ADD			R1,R1,R4	;adds R4 (2) to R1. places result in R1
+			ADD			R1,#1		;adds 1 to R1.  (
 			ADD			R0,#1		;adds 1 to R0.  (0x0001) {first time}
 			SUB			R2,R2,#1	;Subtracts 1 from R1, places result back in R2.
-			CMP			R2,#0		;Compare R2 to 0
 			BNE			Loop		;Returns to Loop if the zero flag is set.  (ie.  the previous operation finally equaled 0).
 			MOVW		R1,#0x0400	;Same as line 10.  ;puts 0x0400 in Register 1. [15:00] 	(0x----0400)
 			MOVT		R1,#0x2000	;puts 0x2000 in Register 1. [31:16]		(0x20000400) {address}
 			MOVB		R2,#0x10	;puts 0x0010 in Register 2. (0x0010)
+			MOVW		R3,#0x0000	;puts 0x0000 in Register 3. (0x0000)
 Sum:		LDR			R0,[R1]		;Loads R0 from the address in R1. (R0 <- 0x20000400) {first time should be 0}
 			ADD			R3,R3,R0	;Adds value in R0 to R3, saves into R3
-			ADD			R1,R1,R4	;Adds 1 to R1
+			ADD			R1,#1		;Adds 1 to R1
 			SUB			R2,R2,#1	;Subtracts 1 from R2 and saves to R2
-			CMP			R2,#0		;Compare R2 to 0
 			BNE			Sum			;Jumps to Sum: if R2 != 0
 			STR			R3,[R1]		;Stores value of R3 in Address in R1
 			B			Prog_Start
