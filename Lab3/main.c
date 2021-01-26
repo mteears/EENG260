538711c2603bfeb3dc821f8bfd966230132aca2a
mteears <50031743+mteears@users.noreply.github.com>
2021-01-25 21:40:22 -0800

    Added momentary function for push button

--- Lab3/main.c
+++ Lab3/main.c
@@ -4,6 +4,7 @@
 # define GPIO_PORTF_DATA_RD         (*(( volatile unsigned long *)0x40025040))
 # define GPIO_PORTF_DATA_WR         (*(( volatile unsigned long *)0x40025020))
 # define GPIO_PORTF_PIN3_EN         0x08
+# define GPIO_PORTF_PIN3_DI         0x00
 # define GPIO_PORTF_PIN4_EN         0x10
 # define SYSTEM_CLOC_FREQUENCY      16000000
 # define DELAY_DEBOUNCE              SYSTEM_CLOC_FREQUENCY/1000
@@ -28,16 +29,21 @@ while(1)
 {
     if(GPIO_PORTF_DATA_RD == 0)
     {
-        Delay(DELAY_DEBOUNCE);
-        if(( flag == 0) && (GPIO_PORTF_DATA_RD == 0))
+        //Delay(DELAY_DEBOUNCE);
+        if(( GPIO_PORTF_DATA_RD  == 0))
         {
-            GPIO_PORTF_DATA_WR ^= GPIO_PORTF_PIN3_EN;
-            flag = 1;
+            if(flag == 0)
+            {
+                GPIO_PORTF_DATA_WR = GPIO_PORTF_PIN3_EN;
+                flag = 1;
+            }
+            if(flag == 1)
+            {
+                GPIO_PORTF_DATA_WR = GPIO_PORTF_PIN3_DI;
+                flag = 0;
+            }
         }
     }
-    else
-    {
-        flag = 0;
-    }
+
 }
 }
