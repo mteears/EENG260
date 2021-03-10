e70598c3d7c0156b272f9bbcb13b295b31aca54c
mteears <50031743+mteears@users.noreply.github.com>
2021-03-10 15:26:08 -0800

    PUSH ALL TO MAIN!  YOLO

---
+++ Project/Debug/main.d
@@ -0,0 +1,7 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../TM4C123GH6PM.h
+
+../main.c: 
+../TM4C123GH6PM.h: 
---
+++ Project/Debug/main1.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main1.obj: ../main1.c
+main1.obj: ../tm4c123gh6pm.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdio.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdarg.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h
+main1.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h
+
+../main1.c: 
+../tm4c123gh6pm.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdio.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdarg.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h: 
